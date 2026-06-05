b8ae0e9af8d5c3648699f105b6f5480b62a6b934
Tyler Williams <tyler.williams@deque.com>
2026-06-05 14:50:36 -0400

    Set up hilt

--- app/src/main/java/com/dubproductions/sudokuchallenge/game/di/GameModule.kt
+++ app/src/main/java/com/dubproductions/sudokuchallenge/game/di/GameModule.kt
@@ -3,37 +3,51 @@ package com.dubproductions.sudokuchallenge.game.di
 import com.dubproductions.sudokuchallenge.game.data.remote.PuzzleApiService
 import com.dubproductions.sudokuchallenge.game.data.repository.PuzzleRepositoryImpl
 import com.dubproductions.sudokuchallenge.game.domain.repository.PuzzleRepository
-import com.dubproductions.sudokuchallenge.game.ui.puzzle.PuzzleScreenViewModel
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.singleOf
-import org.koin.core.module.dsl.viewModelOf
-import org.koin.dsl.module
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
-val gameModule = module {
-    singleOf(::PuzzleRepositoryImpl) { bind<PuzzleRepository>() }
 
-    viewModelOf(::PuzzleScreenViewModel)
-
-    single {
-        Moshi
+@Module
+@InstallIn(SingletonComponent::class)
+object GameModule {
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi
             .Builder()
             .add(KotlinJsonAdapterFactory())
             .build()
     }
 
-    single {
-        Retrofit
+    @Provides
+    @Singleton
+    fun provideRetrofit(moshi: Moshi): Retrofit {
+        return Retrofit
             .Builder()
             .baseUrl("https://you-do-sudoku-api.vercel.app/")
-            .addConverterFactory(MoshiConverterFactory.create(get()))
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
 
-    single {
-        get<Retrofit>().create(PuzzleApiService::class.java)
+    @Provides
+    @Singleton
+    fun providePuzzleApiService(retrofit: Retrofit): PuzzleApiService {
+        return retrofit.create<PuzzleApiService>()
+    }
+
+    @Provides
+    @Singleton
+    fun providePuzzleRepository(puzzleApiService: PuzzleApiService): PuzzleRepository {
+        return PuzzleRepositoryImpl(
+            puzzleApiService = puzzleApiService
+        )
     }
 }
